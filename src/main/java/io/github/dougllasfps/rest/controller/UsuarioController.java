c0c4f0147fc211e5876d83b1d05d42aa2915559e
Vanderlei Pereira <vopereira@gmail.com>
2021-11-25 16:17:28 -0300

    teste Git Vanderlei

---
+++ src/main/java/io/github/dougllasfps/rest/controller/UsuarioController.java
@@ -0,0 +1,53 @@
+package io.github.dougllasfps.rest.controller;
+
+import io.github.dougllasfps.domain.entity.Usuario;
+import io.github.dougllasfps.exception.SenhaInvalidaException;
+import io.github.dougllasfps.rest.dto.CredenciaisDTO;
+import io.github.dougllasfps.rest.dto.TokenDTO;
+import io.github.dougllasfps.security.jwt.JwtService;
+import io.github.dougllasfps.service.impl.UsuarioServiceImpl;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioServiceImpl usuarioService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Inclusão de usuário na Base de dados")
+    public Usuario salvar( @RequestBody @Valid Usuario usuario ){
+        String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(senhaCriptografada);
+        return usuarioService.salvar(usuario);
+    }
+
+    @PostMapping("/auth")
+    @ApiOperation("Retornar dados do Usuario com autenticação")
+    public TokenDTO autenticar(@RequestBody CredenciaisDTO credenciais){
+        try{
+            Usuario usuario = Usuario.builder()
+                    .login(credenciais.getLogin())
+                    .senha(credenciais.getSenha()).build();
+            UserDetails usuarioAutenticado = usuarioService.autenticar(usuario);
+            String token = jwtService.gerarToken(usuario);
+            return new TokenDTO(usuario.getLogin(), token);
+        } catch (UsernameNotFoundException | SenhaInvalidaException e ){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        }
+    }
+
+}
